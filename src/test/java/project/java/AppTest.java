563afaf04f467cd7cf82a8ba5a362054de423846
nithyanagalakshmim <113795341+nithyanagalakshmim@users.noreply.github.com>
2024-03-26 21:15:54 +0530

    Update AppTest.java

--- src/test/java/project/java/AppTest.java
+++ src/test/java/project/java/AppTest.java
@@ -15,7 +15,9 @@ WebDriver driver;
 @BeforeMethod
 public void setup() {
    
+System.setProperty("webdriver.edge.driver", "C:\Users\nithya.m\Downloads\edgedriver_win64\msedgedriver.exe");
         driver = new EdgeDriver();
+	
     
 }
 
